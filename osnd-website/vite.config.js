159d0022159aad0017bbb48150faa7668985d370
Ideasog01 <oliver.dev01@outlook.com>
2025-01-21 12:56:42 +0000

    Deployment Test

--- osnd-website/vite.config.js
+++ osnd-website/vite.config.js
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/opensourcenematodedrugbase.github.io/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
