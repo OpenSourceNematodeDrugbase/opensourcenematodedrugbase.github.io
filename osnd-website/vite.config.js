d8b293413b849c169dda28512f88066982a5e496
Ideasog01 <oliver.dev01@outlook.com>
2025-01-21 12:51:44 +0000

    Update (GitHub Pages)

--- osnd-website/vite.config.js
+++ osnd-website/vite.config.js
@@ -6,9 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/opensourcenematodedrugbase.github.io/'
-    : '/',
+  base: '/opensourcenematodedrugbase.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
